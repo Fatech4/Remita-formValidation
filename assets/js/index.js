d480d89c143af87d76116b5d0422cfcb3a16cb12
Fatech4 <adekoladamilola4@gmail.com>
2020-08-16 18:07:20 -0400

    Js Validation

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+const error = document.querySelectorAll('#error');
+const account = document.getElementById('accountType');
+const labelChange = document.querySelector('[for=corporateName]');
+const corporate = document.getElementById('corporateName');
+const gov = document.querySelector('.gov');
+const form = document.getElementsByTagName('form')[0];
+
+account.addEventListener('change', function() {
+    if (account.value == "PERSONAL") {
+        document.querySelector('.hidden').style.display = "none";
+
+    } else if (account.value == "SME") {
+        labelChange.textContent = "Micro SME Name "
+        corporate.placeholder = "Please Enter your Micro SME Name"
+        gov.style.display = "block";
+        document.querySelector('.hidden').style.display = "block";
+
+    } else if (account.value == "CORPORATE") {
+        labelChange.textContent = "Corporate/SME Name "
+        corporate.placeholder = "Please Enter your Corporate/SME Name"
+        gov.style.display = "block";
+        document.querySelector('.hidden').style.display = "block";
+
+
+    } else if (account.value == "GOVERNMENT") {
+        document.querySelector('.hidden').style.display = "block";
+
+        gov.style.display = "none";
+        labelChange.textContent = "Government/MDA Name "
+        corporate.placeholder = "Please Enter your Government/MDA Name"
+    } else {
+
+    }
+
+})
+
+
+form.addEventListener('submit', function(e) {
+    const req = document.querySelectorAll('[required]');
+    for (i = 0; i < req.length; i++) {
+        if (req[i].validity.valid) {
+            error[i].innerHTML = "";
+            req[i].style.border = "inherit";
+        } else {
+            e.preventDefault();
+            error[i].innerHTML = "The field is required";
+            req[i].style.border = "2px solid red";
+            req[i].style.background = "#ffe4dc";
+
+        }
+    }
+
+});
