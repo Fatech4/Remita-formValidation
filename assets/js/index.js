342b556ff2e69d7357cda60009d2b8ef44b6be01
Fatech4 <adekoladamilola4@gmail.com>
2020-08-17 02:13:03 -0400

    Js validation review

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,10 @@
-const error = document.querySelectorAll('#error');
+//const error = document.querySelectorAll('#error');
 const account = document.getElementById('accountType');
 const labelChange = document.querySelector('[for=corporateName]');
 const corporate = document.getElementById('corporateName');
 const gov = document.querySelector('.gov');
 const form = document.getElementsByTagName('form')[0];
+
 
 account.addEventListener('change', function() {
     if (account.value == "PERSONAL") {
@@ -31,7 +32,106 @@ account.addEventListener('change', function() {
     } else {}
 
 })
-const req = document.querySelectorAll('[required]');
+
+
+function fvalidation() {
+    const fName = document.getElementById('fname');
+    const lName = document.getElementById('lname');
+    const phone = document.getElementById('phone');
+    const account = document.getElementById('accountType');
+    const email = document.getElementById('email')
+    var fnameErr = lnameErr = phoneErr = emailErr = accountErr = true;
+
+    if (email.validity.valid) {
+        error = document.querySelector('#email + label.error');
+        error.textContent = "";
+        error.style.border = "inherit";
+
+    } else {
+        error = document.querySelector('#email + label.error');
+        error.innerHTML = "The field is required";
+        email.style.border = "2px solid red";
+        email.style.background = "#ffe4dc";
+        emailErr = false;
+    }
+
+    if (fName.validity.valid) {
+        error = document.querySelector('#fname + label.error');
+        error.textContent = "";
+        error.style.border = "inherit";
+
+    } else {
+        error = document.querySelector('#fname + label.error');
+        error.innerHTML = "The field is required";
+        fName.style.border = "2px solid red";
+        fName.style.background = "#ffe4dc";
+        fnameErr = false;
+    }
+    if (lName.validity.valid) {
+        error = document.querySelector('#lname + label.error');
+        error.textContent = "";
+        error.style.border = "inherit";
+
+
+    } else {
+        error = document.querySelector('#lname + label.error');
+        error.innerHTML = "The field is required";
+        lName.style.border = "2px solid red";
+        lName.style.background = "#ffe4dc";
+        lnameErr = false;
+    }
+    if (phone.validity.valid) {
+        error = document.getElementById('error');
+        error.textContent = "";
+        error.style.border = "inherit";
+
+
+    } else {
+        error = document.getElementById('error');
+
+        error.textContent = "The field is required";
+        phone.style.border = "2px solid red";
+        phone.style.background = "#ffe4dc";
+        phoneErr = false;
+    }
+    if (phone.validity.valid) {
+        error = document.querySelector('#accountType + label.error');
+        error.textContent = "";
+        error.style.border = "inherit";
+
+
+    } else {
+        error = document.querySelector('#accountType + label.error');
+        error.textContent = "The field is required";
+        account.style.border = "2px solid red";
+        account.style.background = "#ffe4dc";
+        accountErr = false;
+    }
+
+
+    if ((emailErr || fnameErr || lnameErr || phoneErr || accountErr) == false) {
+        return false;
+    } else {
+        alert("Form Submitted Successfully");
+    }
+}
+
+
+//form.onsubmit = validation();
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    fvalidation();
+    /*  event.preventDefault(); */
+    /*  if (fvalidation() == true) {
+         alert("hvhvhvhv");
+         return true;
+     } else {
+         event.preventDefault();
+         return false;
+     }  */
+})
+
+/* onst req = document.querySelectorAll('[required]');
 req.forEach(function(field) {
     field.addEventListener('input', function() {
         if (field.validity.valid) {
@@ -51,7 +151,7 @@ form.addEventListener('submit', function(e) {
         if (req[i].validity.valid) {
             error[i].innerHTML = "";
             req[i].style.border = "inherit";
-            window.alert("Form Submitted Sucessfully");
+
         } else {
             e.preventDefault();
             error[i].innerHTML = "The field is required";
@@ -61,4 +161,4 @@ form.addEventListener('submit', function(e) {
         }
     }
 
-});
+}); */
